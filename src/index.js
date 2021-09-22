dd7e4adf6b86f734f37229513bf1a91f899f0e89
Dmitry Bogomolov <baga@Dmitrys-MacBook-Air.local>
2021-09-22 18:30:32 +0300

    use component lifecycle methods

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // onetime setup
+    constructor(props) {
+        super(props);
+        this.state = { latitude: 0, longitude: 0 , errorMessage: ''}; // the only time state is set directly
+
+    }
+    render() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.latitude) {
+            return <div>Latitude: {this.state.latitude}</div>
+        }
+        return <div>Loading...</div>
+    }
+    // data loading
+    // start tick updates
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }),
+            error => this.setState({errorMessage: error.message})
+            
+        );
+        console.log('App component was added to the DOM')
+    }
+
+    // data loading on props/state change
+    componentDidUpdate() {
+        console.log('App component was updated')
+    }
+
+    componentWillUnmount() {
+
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
